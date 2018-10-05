32ed40f6fc016bca2f33a5e80142c4b2c5efedc8
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-10-05 17:36:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,7 +34,7 @@ end
 def kesha_maker(array)
   array.collect! do |string|
     # string.insert(2, "$")
-    string.chars.insert(2, "$")
+    string.join << string.chars[2] = "$"
   end
   array
 end
